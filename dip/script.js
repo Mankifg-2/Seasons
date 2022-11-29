1f38cfd3364f5e5a78d32e4e119216ee875af14a
Mankifg-2 <jaka.horvat@ospuconci.si>
2022-11-29 13:49:10 +0100

    better display/phone

---
+++ dip/script.js
@@ -0,0 +1,29 @@
+document.onkeydown = checkKey;
+
+var i = 1
+
+var source = "./dip"
+
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '37') {
+       i = i - 1
+       work(i)
+
+    }
+    else if (e.keyCode == '39') {
+        i = i + 1
+        work(i)
+    }
+
+}
+
+
+function work(index) {
+    console.log(i);
+    document.getElementById("img").src = source.concat(index.toString(), ".jpg");
+    console.log(source.concat(index.toString(), ".jpg"));
+}
