2b623d13e43e0d9dbbd0ebbdc371663ea9bdd1b7
Mankifg-2 <jaka.horvat@ospuconci.si>
2022-11-29 13:55:46 +0100

    i hope its last time

--- dip/script.js
+++ dip/script.js
@@ -1,9 +1,7 @@
 document.onkeydown = checkKey;
 
 var i = 1
-
 var source = "./dip"
-
 
 function checkKey(e) {
 
@@ -11,17 +9,17 @@ function checkKey(e) {
 
     if (e.keyCode == '37') {
        i = i - 1
+       if (i<1) {i = i +1}
        work(i)
 
     }
     else if (e.keyCode == '39') {
         i = i + 1
+        if(i>6) {i = i - 1}
         work(i)
     }
 
 }
-
-
 function work(index) {
     console.log(i);
     document.getElementById("img").src = source.concat(index.toString(), ".jpg");
